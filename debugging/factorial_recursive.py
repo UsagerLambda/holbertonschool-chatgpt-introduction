39b4c90da93f5f98901fb84c233925b3c442d773
UsagerLambda <clement.chassem@gmail.com>
2024-09-02 11:04:17 +0200

    add \n in factorial_recursive.py

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
     """
     Function Description:
@@ -17,6 +18,7 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
 
 # Calculate the factorial of the number provided as a command-line argument
 f = factorial(int(sys.argv[1]))
